49286f932ec5e5adfefe09db0fd786015d688ec7
briginmis <brianne_ng@y7mail.com>
2022-01-17 22:01:07 +0800

    create sqlite files and setup database in app.py

--- app.py
+++ app.py
@@ -2,28 +2,26 @@ from flask import Flask, render_template
 from sqlalchemy import create_engine
 
 # database setup 
-engine = create_engine("sqlite:///titanic.sqlite")
+engine_movies = create_engine("sqlite:///movie_data.sqlite")
+engine_country = create_engine("sqlite:///country_data.sqlite")
+engine_genre = create_engine("sqlite:///genre_data.sqlite")
+engine_languages = create_engine("sqlite:///languages_data.sqlite")
 
 # flask app setup
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("templates/index.html")
 
-@app.route("/api/names")
+@app.route("/statsbygenre")
 def names():
-    results = [
-        {
-            "id": list(row)[0],
-            "name": list(row)[1],
-            "pclass": list(row)[2],
-            "age": list(row)[3],
-            "sex": list(row)[4],
-            "survived": list(row)[5],
-        } for row in engine.execute("select * from passenger").all()]
+    return render_template("templates/stats_genre.html")
 
-    return {"passengers":results}
+@app.route("/statsbycountry")
+def names():
+    return render_template("templates/stats_country.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
